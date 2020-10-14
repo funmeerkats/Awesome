51e142c327fb162ae837f0363f215ed8ebb8998a
AlekseyKutsko <alexej.kutsko@gmail.com>
2020-10-14 20:58:37 +0300

    test3 commit1

--- sagas.js
+++ sagas.js
@@ -1,7 +1,5 @@
 import { all, call, fork, put, take, putResolve, takeEvery, takeLatest, select } from 'redux-saga/effects';
 
-console.log(1);
-
 const delay = (ms) => {
     return new Promise((resolve) => {
         setTimeout(() => resolve(), ms);
