a6bff6e20197b54936991176b9df3fc4725912ac
AlekseyKutsko <alexej.kutsko@gmail.com>
2020-10-15 18:30:37 +0300

    TS was added.

--- Thunk.tsx
+++ Thunk.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {actionCreatorForSaga} from './asyncReducer';
+import {createAsyncActionHelper, createNotAsyncActionHelper} from './asyncReducer';
 
 import {
     View,
@@ -10,35 +10,41 @@ import {
 
 let asyncCount = 0;
 let notAsyncCount = 0;
-const Saga = (props) => {
+
+interface stateObj {
+    asyncDataThunk: number
+}
+
+const Thunk = (props: {}) => {
+
     const dispatch = useDispatch();
-    const sagaData = useSelector(state => state.asyncDataSaga);
+    const thunkData = useSelector((state: stateObj): number => state.asyncDataThunk);
 
     useEffect(() => {
-        console.log('render saga', sagaData);
-    }, [sagaData]);
+        console.log('render thunk', thunkData);
+    }, [thunkData]);
 
     const actionClick = () => {
         asyncCount += 10;
-        dispatch(actionCreatorForSaga('SAGA_REQUEST', {async: true, count: asyncCount}));
+        dispatch(createAsyncActionHelper(asyncCount));
     };
 
     const actionNotAsyncClick = () => {
         notAsyncCount += 20;
-        dispatch(actionCreatorForSaga('SAGA_REQUEST', {async: false, count: notAsyncCount}));
+        dispatch(createNotAsyncActionHelper(notAsyncCount));
     };
 
     return (
         <View style={{marginVertical: 20, borderWidth: 1, borderColor: 'blue'}}>
-            <Text>Saga</Text>
+            <Text>Thunk</Text>
             <TouchableOpacity onPress={actionClick}>
-                <Text style={{textAlign: 'center'}}>Send async Saga</Text>
+                <Text style={{textAlign: 'center'}}>Send async Thunk</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={actionNotAsyncClick}>
-                <Text style={{textAlign: 'center'}}>Send not async Saga</Text>
+                <Text style={{textAlign: 'center'}}>Send not async Thunk</Text>
             </TouchableOpacity>
         </View>
     )
 };
 
-export {Saga}
+export {Thunk}
