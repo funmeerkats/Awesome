fb2ce7b9b767c16e0935bff1b86a61f6ee412d7d
AlekseyKutsko <alexej.kutsko@gmail.com>
2020-10-22 18:07:28 +0300

    TS was added(reducer).

--- App.tsx
+++ App.tsx
@@ -18,18 +18,40 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import { createStore, applyMiddleware } from 'redux';
+import createRouter from './createRouter'
 import reducers from './reducersMain';
 import MySaga from './sagas';
 import {loggingMiddleware} from './customMiddleWare';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+const Drawer = createDrawerNavigator();
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+import {Thunk} from './Thunk';
 
 import {MainComponent} from './Main';
+import {Main2Component} from './Main2';
 
 const isMomHappy = true;
+
+// declare var alert: (param: string) => void;
+
+// const router = createRouter();
 
 const App = () => {
     const sagaMiddleware = createSagaMiddleware();
     const store = createStore(reducers, applyMiddleware(thunk, sagaMiddleware, loggingMiddleware));
     sagaMiddleware.run(MySaga);
+
+    const mergeObject = <T, R>(a: T, b: R): T & R => {
+        return Object.assign({}, a, b);
+    };
+    const merged1 = mergeObject({name: 'Alex'}, {age: 30});
+    const merged2 = mergeObject(122, 22);
+    console.log(merged1);
 
 // Промис
     const willIGetNewPhone = new Promise<PhoneValue>(
@@ -182,72 +204,122 @@ const App = () => {
         return prev;
     };
 
-    // class Animal {
-    //     constructor(name){
-    //         this.name = name;
-    //     }
-    //
-    //     getName () {
-    //         console.log(`Animal's name is ${this.name}`)
-    //     }
-    // }
-    //
-    // class Cat extends Animal {
-    //     constructor(name, color){
-    //         super(name);
-    //         this.color = color;
-    //     
-    //
-    //     voice(){
-    //         console.log(`${this.name} says ${this.color}`);
-    //     };
-    // }
+    interface IUser {
+        name: string,
+        sayHi: () => void
+    }
+
+    class User implements IUser{
+        name: string = "Аноним";
+
+        constructor(name?: string){
+            if(name){
+                this.name = name;
+            }
+        }
+
+        sayHi() {
+            console.log(`Привет, ${this.name}!`);
+        }
+    }
+
+    const user = new User();
+
+    class childUser extends User {
+        color: string;
+
+        constructor(color: string, name?: string){
+            super(name);
+            this.color = color;
+        }
+
+        changeNamePerson(name: string){
+            this.name = name
+        }
+
+        sayHiChild(){
+            console.log(`Привет2, ${this.name}!`)
+        }
+    }
+
+    const child = new childUser('Red', 'Lex2222');
+    // child.changeNamePerson('Lex');
+
+    // user.sayHi();
+    // child.sayHiChild();
 
     interface AnimalObj {
         name: string,
         [key: string]: any
     }
 
-    const Animal = function(this: AnimalObj, name: string){
-        this.name = name;
-    };
-    Animal.prototype.getName = function(){
-        console.log(`Animal's name is ${this.name}`);
-    };
+    class Animal implements AnimalObj {
+        name: string;
 
-    interface CatObj {
-        name: string,
-        color: string,
-        [key: string]: any
+        constructor(name: string){
+            this.name = name;
+        }
+
+        getName () {
+            console.log(`Animal's name is ${this.name}`)
+        }
     }
 
-    const Cat = function(this: CatObj, name: string, color: string){
-        // this.constructor.super.call(this, name);
-        Animal.call(this, name);
-        this.color = color;
-    };
+    class Cat extends Animal implements AnimalObj{
+        color: string;
 
-    function inherit(child: CatObj, parent: AnimalObj){
-        let F = function(){};
-        F.prototype = parent.prototype;
-        child.prototype = new (F as any)();
-        child.prototype.constructor = child;
-        child.super = parent;
+        constructor(name: string, color: string) {
+            super(name);
+            this.color = color;
+        }
+
+        voice(){
+            console.log(`${this.name} says ${this.color}`);
+        };
     }
-    // inherit(Cat, Animal);
-    Object.setPrototypeOf(Cat.prototype, Animal.prototype);
 
-    Cat.prototype.voice = function(){
-        console.log(`${this.name} says ${this.color}`);
-    };
+    //
+    // const Animal = function(this: AnimalObj, name: string){
+    //     this.name = name;
+    // };
+    // Animal.prototype.getName = function(){
+    //     console.log(`Animal's name is ${this.name}`);
+    // };
+    //
+    // interface CatObj {
+    //     name: string,
+    //     color: string,
+    //     [key: string]: any
+    // }
+    //
+    // const Cat = function(this: CatObj, name: string, color: string){
+    //     // this.constructor.super.call(this, name);
+    //     Animal.call(this, name);
+    //     this.color = color;
+    // };
+    //
+    // function inherit(child: CatObj, parent: AnimalObj){
+    //     let F = function(){};
+    //     F.prototype = parent.prototype;
+    //     child.prototype = new (F as any)();
+    //     child.prototype.constructor = child;
+    //     child.super = parent;
+    // }
+    // // inherit(Cat, Animal);
+    // Object.setPrototypeOf(Cat.prototype, Animal.prototype);
+    //
+    // Cat.prototype.voice = function(){
+    //     console.log(`${this.name} says ${this.color}`);
+    // };
 
     const xxx2 = () => {
-        const myCat = new (Cat as any)('FFF', 'RED');
+        const myCat = new Cat('FFF', 'RED');
 
         myCat.voice();
 
     };
 
+    const variable: [number, string] = [11234, '1234'];
 
 
     const request = async () => {
@@ -269,22 +341,28 @@ const App = () => {
     return (
         <>
             <StatusBar barStyle="dark-content"/>
-            <SafeAreaView>
-                <TouchableOpacity onPress={click}>
-                    <Text>Ask</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => bubbleSort([3,5,2,1,0,4,7,2])}>
-                    <Text>sort</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={xxx2}>
-                    <Text>xxx2</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => fib(4)}>
-                    <Text>fib</Text>
-                </TouchableOpacity>
+            <SafeAreaView style={{flex: 1}}>
+                {/*<TouchableOpacity onPress={click}>*/}
+                {/*    <Text>Ask</Text>*/}
+                {/*</TouchableOpacity>*/}
+                {/*<TouchableOpacity onPress={() => bubbleSort([3,5,2,1,0,4,7,2])}>*/}
+                {/*    <Text>sort</Text>*/}
+                {/*</TouchableOpacity>*/}
+                {/*<TouchableOpacity onPress={xxx2}>*/}
+                {/*    <Text>xxx2</Text>*/}
+                {/*</TouchableOpacity>*/}
+                {/*<TouchableOpacity onPress={() => fib(4)}>*/}
+                {/*    <Text>fib</Text>*/}
+                {/*</TouchableOpacity>*/}
 
                 <Provider store={store}>
-                    <MainComponent />
+                    <NavigationContainer>
+                        <Drawer.Navigator initialRouteName="Main">
+                            <Drawer.Screen name="Main" component={MainComponent} />
+                            <Drawer.Screen name="Main2" component={Main2Component} />
+                        </Drawer.Navigator>
+                    </NavigationContainer>
+                    {/*<MainComponent />*/}
                 </Provider>
             </SafeAreaView>
         </>
