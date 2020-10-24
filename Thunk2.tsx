b19c8a8f8b6159dee25578dc819fcadf00c67f22
AlekseyKutsko <alexej.kutsko@gmail.com>
2020-10-24 11:54:18 +0300

    testtest commit

--- Thunk2.tsx
+++ Thunk2.tsx
@@ -84,25 +84,6 @@ const Thunk2 = (props: {}) => {
 
     }
 
-    function memoize2(func: (...args: number[]) => number) {
-        // TODO: implement function
-        let cache: {[key: string]: number | string} = {};
-
-        return function (...args: number[]) {
-            const result = func(...args);
-
-            if (result === cache[result]) {
-                // return memoize;
-            }
-
-            cache[result] = result;
-
-            return cache;
-
-        }
-
-    }
-
     interface IActionReducer {
         type?: string,
         action?: number | string | object
