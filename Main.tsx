83194253f157ad1dc314443b06b4635750d2e499
AlekseyKutsko <alexej.kutsko@gmail.com>
2022-01-08 21:24:12 +0300

    test

--- Main.tsx
+++ Main.tsx
@@ -1,31 +1,34 @@
-import React, {useCallback, useMemo, useRef, useState, useEffect} from 'react';
-import {Thunk} from './Thunk';
-import {MainScreenComponent} from './MainScreenComponent';
+import React, {
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+  useEffect,
+} from "react";
+import { Thunk } from "./Thunk";
+import { MainScreenComponent } from "./MainScreenComponent";
 
-import {
-    Button
-} from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 const Stack = createStackNavigator();
 
 const MainComponent = (props: any) => {
-    const {navigation} = props;
+  const { navigation } = props;
 
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="MainTitle1" component={MainScreenComponent}
-            options={{
-              headerLeft: () => (
-                  <Button
-                      onPress={() => navigation.openDrawer()}
-                      title="Menu"
-                  />
-              ),
-            }}
-            />
-            <Stack.Screen name="Thunk" component={Thunk} />
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="MainTitle1"
+        component={MainScreenComponent}
+        options={{
+          headerLeft: () => (
+            <Button onPress={() => navigation.openDrawer()} title="Menu" />
+          ),
+        }}
+      />
+      <Stack.Screen name="Thunk" component={Thunk} />
+    </Stack.Navigator>
+  );
 };
 
-export {MainComponent}
+export { MainComponent };
